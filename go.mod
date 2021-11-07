ec84a34a38694abc8d152ecf458872873750d3f0
ColeJSmith19 <colesmith1337@gmail.com>
2021-11-07 11:27:51 -0700

    mtgSpoilers bot is now sending the newest n cards to the discord server

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ColeJSmith19/mtgSpoilers
 
 go 1.16
 
-require github.com/bwmarrin/discordgo v0.23.2 // indirect
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/bwmarrin/discordgo v0.23.2 // indirect
+)
--- main.go
+++ main.go
@@ -3,17 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/bwmarrin/discordgo"
 )
 
 // Variables used for command line parameters
 var (
 	Token string
+)
+
+var (
+	ActiveSession *discordgo.Session
+	ActiveMessage *discordgo.MessageCreate
+	SpoilerCount  int    = 0
+	MTGBoisRole   string = "<@&802701848619778098>"
 )
 
 func init() {
@@ -54,7 +64,6 @@ func main() {
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the autenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
 	// Ignore all messages created by the bot itself
 	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
@@ -77,4 +86,92 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
+	if text[1] == "check" {
+		ActiveMessage = m
+		ActiveSession = s
+		checkForNewSpoilers()
+	}
+
 }
+
+func checkForNewSpoilers() string {
+	scrapeMythicSpoiler()
+
+	return ""
+}
+
+func scrapeMythicSpoiler() {
+	response, err := http.Get("http://mythicspoiler.com/newspoilers.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	// Create a goquery document from the HTTP response
+	document, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal("Error loading HTTP response body. ", err)
+	}
+
+	// Find all links and process them with the function
+	// defined earlier
+	document.Find("img").Each(processElement)
+}
+
+// This will get called for each HTML element found
+func processElement(index int, element *goquery.Selection) {
+	// See if the src attribute exists on the element
+	img, exists := element.Attr("src")
+	if exists {
+		if strings.Contains(img, "/cards/") {
+			if SpoilerCount > 5 {
+				return
+			}
+			imageSource := fmt.Sprintf("%s\nhttp://mythicspoiler.com/%s", MTGBoisRole, strings.Trim(img, "\n"))
+			ActiveSession.ChannelMessageSend(ActiveMessage.ChannelID, imageSource)
+			SpoilerCount++
+		}
+	}
+}
+
+// scraped HTML card examples
+
+// <!--CARD CARD CARD CARD CARD CARD CARD--><div class="grid-card"><a href="
+// mid/cards/nebelgastintruder.html
+// "><img class="midcard" src="
+// mid/cards/nebelgastintruder.jpg
+// "></a><!--URL BELOW--><a href="
+// https://twitter.com/coL_AliasV/status/1434911305379504129
+// "><center><font face="'Arial Black', Gadget, sans-serif" color="#555555" size="-4">
+// AliasV
+// </font></a> </div><!--END CARD-->
+
+//         <!--CARD CARD CARD CARD CARD CARD CARD--><div class="grid-card"><a href="
+// mid/cards/covertcutpurse.html
+// "><img class="midcard" src="
+// mid/cards/covertcutpurse.jpg
+// "></a><!--URL BELOW--><a href="
+// https://twitter.com/val_pl_mafr/status/1434901009394974725?s=20
+// "><center><font face="'Arial Black', Gadget, sans-serif" color="#555555" size="-4">
+// @val_pl_mafr
+// </font></a> </div><!--END CARD-->
+
+//         <!--CARD CARD CARD CARD CARD CARD CARD--><div class="grid-card"><a href="
+// mid/cards/covertcutpurse.html
+// "><img class="" src="
+// mid/cards/covetousgeist.jpg
+// "></a><!--URL BELOW--><a href="
+// https://twitter.com/val_pl_mafr/status/1434901009394974725?s=20
+// "><center><font face="'Arial Black', Gadget, sans-serif" color="#555555" size="-4">
+// @val_pl_mafr
+// </font></a> </div><!--END CARD-->
+
+//         <!--CARD CARD CARD CARD CARD CARD CARD--><div class="grid-card"><a href="
+// mid/cards/burlybreaker.html
+// "><img class="midcard" src="
+// mid/cards/burlybreaker1.jpg
+// "></a><!--URL BELOW--><a href="
+// https://twitter.com/Blackneto_/status/1434895795967254532?s=20
+// "><center><font face="'Arial Black', Gadget, sans-serif" color="#555555" size="-4">
+// @Blackneto_
+// </font></a> </div><!--END CARD-->
